df6d6e792e81b06e0d9f0ba3c071fc1a9b04b81f
Truong Quy <nguyentruongquy75@gmail.com>
2022-01-09 14:58:51 +0700

    Fix PDF

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import Viewer, { Worker } from "@phuocng/react-pdf-viewer";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
 import CV from "../../assets/cv/CV.pdf";
 
-import "@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css";
+// Create new plugin instance
 
 export default function Resume() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
-    <div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
-        <div style={{ height: "750px" }}>
-          <Viewer fileUrl={CV} />
-        </div>
-      </Worker>
-    </div>
+    <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.worker.min.js">
+      <div
+        style={{
+          height: "750px",
+        }}
+      >
+        <Viewer fileUrl={CV} plugins={[defaultLayoutPluginInstance]} />
+      </div>
+    </Worker>
   );
 }
